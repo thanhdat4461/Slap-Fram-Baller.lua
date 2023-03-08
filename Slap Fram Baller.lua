fe8f0d416b94ad44d15ad5cde6f9e6db9e00f58f
thanhdat4461 <125394392+thanhdat4461@users.noreply.github.com>
2023-03-08 23:05:33 +0700

    Create Slap Fram Baller.lua

---
+++ Slap Fram Baller.lua
@@ -0,0 +1,135 @@
+local SafeSpot = Instance.new("Part", workspace)
+
+SafeSpot.Position = Vector3.new(-1500,100,-1500)
+
+SafeSpot.Name = "Spot"
+
+SafeSpot.Size = Vector3.new(50,50,50)
+
+SafeSpot.Anchored = true
+
+SafeSpot.Transparency = .7
+
+wait(.2)
+
+task.wait(.2)
+
+fireclickdetector(game:GetService("Workspace").Lobby.Baller.ClickDetector)
+
+task.wait(.2)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Lobby.Teleport1.CFrame
+
+task.wait(1)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Spot.CFrame * CFrame.new(0,50,0)
+
+    
+
+    local GaySexWithBori =
+
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/cheesynob39/Coasting/main/Source.lua")))()
+
+    
+
+        local CUMBAT = GaySexWithBori:CreateTab("Autofarm")
+
+    local CUMBAT3 = CUMBAT:CreateSection("Slaps")
+
+    
+
+    
+
+    CUMBAT3:CreateToggle("Autofarm", function(bool)
+
+        bully = bool
+
+        
+
+        if bool == true then
+
+            
+
+            BooleanTing()
+
+            
+
+            end
+
+        
+
+        
+
+        
+
+        
+
+    end)
+
+    
+
+    CUMBAT3:CreateButton("Autofarm", function(bool)
+
+   
+
+    for i = 1,55 do
+
+        
+
+game:GetService("ReplicatedStorage").GeneralAbility:FireServer()
+
+end
+
+        
+
+        
+
+        
+
+        
+
+        
+
+        
+
+    end)
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    function BooleanTing()
+
+        
+
+              while bully do
+
+        
+
+        wait(.001)
+
+    
+
+    for _, v in ipairs(workspace:GetChildren()) do
+
+    
+
+    if string.sub(v.Name, 3, 8) == "Baller" then
+
+game:GetService("ReplicatedStorage").GeneralHit:FireServer(v:WaitForChild("HumanoidRootPart"))
+
+end
+
+end
+
+end
+
+        
+
+    end
